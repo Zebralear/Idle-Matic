83a35188cf67f3296b30456bf15293985611f4ad
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-10 17:44:58 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,9 @@
 function mining() {
-  document.getElementById(output).innerHTML("mining works");
+  document.getElementById("output").innerHTML("mining works");
+}
+function explorer() {
+  document.getElementById("output").innerHTML("explorer works");
+}
+function inventor() {
+  document.getElementById("output").innerHTML("inventor works");
 }
