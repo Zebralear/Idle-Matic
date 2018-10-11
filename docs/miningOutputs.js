7975711bbc9ad694a01affd347b70f8ff99cbba5
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-11 13:38:53 -0500

    Added some Mechanics
    Added some functions to allow the game to give the player resources

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,1 +1,19 @@
+function outputTimer() {
+  window.setInterval(RNG, 100);
+  window.setInterval(debug, 100);
+  window.setInterval(outputDeterminer, 100);  
+}
+  
+function RNG() {
+  var output= new Math.floor((Math.random()*10)+1);
+}
 
+function debug() {
+  document.getElementById("debugOutput").innerHTML = "output";
+}
+
+function outputDeterminer() {
+  if (output!=1) {
+    var currentStone= currentStone + 1;
+    }
+}
