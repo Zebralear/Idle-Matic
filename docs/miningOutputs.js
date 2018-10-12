eba9a07238588d79685fb53bc492599afe211b90
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-12 08:52:52 -0500

    Integrated debugger into Output Determiner

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -2,10 +2,6 @@ function outputTimer() {
   window.setInterval(outputDebug, 100);
   window.setInterval(outputDeterminer, 100);  
 }
-  
-function outputDebug() {
-  document.getElementById("debugOutput").innerHTML = output;
-}
 
 function outputDeterminer() {
   var output = new Math.floor((Math.random()*100)+1);
@@ -13,4 +9,5 @@ function outputDeterminer() {
     var currentStone = currentStone + 1;
     }
   document.getElementById("visibleOutput").innerHTML = currentStone;
+  document.getElementById("debugOutput").innerHTML = output;
 }
