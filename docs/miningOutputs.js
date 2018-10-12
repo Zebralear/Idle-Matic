4dd8e22c924766bf9f5f7588ce0517550c263856
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-12 12:41:46 -0500

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,14 +1,13 @@
 function outputTimer() {
   window.setInterval(outputDebug, 100);
   window.setInterval(outputDeterminer, 100);  
-}
+}e
 
 function outputDeterminer() {
-  var output = new Math.floor((Math.random()*100)+1);
-  /*
+  var output = Math.floor((Math.random()*100)+1);
   if (output != 1) {
     var currentStone = currentStone + 1;
-    } */
+    }
   //document.getElementById("visibleOutput").innerHTML = currentStone;
   document.getElementById("debugOutput").innerHTML = output;
 }
