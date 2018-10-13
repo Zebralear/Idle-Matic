c46a4a4d23cbb0747ee510b1e85b68fcb23b3a5c
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-13 15:40:23 -0500

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -7,11 +7,12 @@ function outputTimer() {
 function outputDeterminer() {
   if (currentStone !== "undefined") {
     var currentStone = 0;
-  }
+  } else {
   var output = Math.floor((Math.random()*100)+1);
   if (output != 1) {
     var currentStone = currentStone + 1;
-    }
+                   }
+         }
   document.getElementById("visibleOutput").innerHTML = currentStone;
   document.getElementById("debugOutput").innerHTML = output;
 }
