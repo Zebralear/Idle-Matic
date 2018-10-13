8461d03b2ca1243d21f486c9ea9dc53b2bb09f2a
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-13 16:30:42 -0500

    Update mining.js
    Attempting to change background color instead of text

--- docs/mining.js
+++ docs/mining.js
@@ -1,8 +1,8 @@
 function drillLoad() {
   $("#classInternalPlacer").load("miningDrillInternal.html");
-  document.getElementById("classInternalPlacer").style.color = "brown";
+  document.getElementById("classInternalPlacer").style.background.color = "brown";
 }
 function miningUpgradeLoad() {
   $("#classInternalPlacer").load("miningUpgradeInternal.html");
-  document.getElementById("classInternalPlacer").style.color = "brown";
+  document.getElementById("classInternalPlacer").style.background.color = "brown";
 }
