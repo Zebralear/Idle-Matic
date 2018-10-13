76f66144b0da6a5aa51ccdad2bb09df5b0887d4b
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-13 15:56:35 -0500

    Added some JS DOM CSS
    Added script to change the background of the classInternalPlacer to brown when Mining tabs are selected

--- docs/mining.js
+++ docs/mining.js
@@ -1,6 +1,8 @@
 function drillLoad() {
   $("#classInternalPlacer").load("miningDrillInternal.html");
+  document.getElementById("classInternalPlacer").style.color = "brown";
 }
 function miningUpgradeLoad() {
   $("#classInternalPlacer").load("miningUpgradeInternal.html");
+  document.getElementById("classInternalPlacer").style.color = "brown";
 }
