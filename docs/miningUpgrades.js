891fb5f653a3b7a4d8d4c3215c6eb6c4d6c66d36
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-15 09:25:49 -0500

    First commit for upgrade functions
    Added the first wave of functions to get upgrades to work properly.

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -1,1 +1,28 @@
+function varSet() {
+  var drillNumber = 1;
+  var drillSpeed = 1;
+  var drillRarity = 1;
+  var drillDoubleDrops= 0;
+  var unlockedSpeed = 0;
+  var unlockedRarity = 0;
+  var unlockedDoubleDrops = 0;
+  var dropsPerSecond = 1;
+  var money = 100000000;
+}
 
+function buyExtra() {
+  if (money >= 1000*drillNumber) {
+    drillNumber = drillNumber + 1;
+    money = money-(1000*(drillNumber-1));
+    dropsPerSecond = drillNumber*(drillSpeed+1);
+  }
+  if (drillNumber == 2) {
+    unlockedSpeed = 1;
+    document.getElementById("drillSpeedUpgrades").style.display = "block";
+    }
+  
+}
+
+function buySpeed() {
+  
+}
