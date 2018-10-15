15d63d5efd86f8a64b267e50e436b298c69838af
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-15 09:55:48 -0500

    Attempted to fix variables
    Added a function to run onLoad to define the variables.

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,3 +1,7 @@
+function outputLoad() {
+  var output = 0;
+  var currentStone = 0;
+
 function outputTimer() {
   window.setInterval(outputDebug(), 100);
   window.setInterval(outputDeterminer(), 100);
@@ -5,14 +9,10 @@ function outputTimer() {
 }
 
 function outputDeterminer() {
-  if (currentStone !== undefined) {
-    var currentStone = 0;
-  } else {
-  var output = Math.floor((Math.random()*100)+1);
+  output = Math.floor((Math.random()*100)+1);
   if (output != 1) {
     currentStone = currentStone + 1;
-                   }
-         }
+  }
   document.getElementById("visibleOutput").innerHTML = currentStone;
   document.getElementById("debugOutput").innerHTML = output;
 }
