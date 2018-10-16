bda83641c826c401d59b48acee93878da385dd1e
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-16 08:59:31 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,7 @@
 function mining() {
+  document.getElementById("classPlacer").style.display = "block";
   $("#classPlacer").load("miningTabControl.html");
-  var loadedPage = 1
+  var loadedPage = 1;
 }
 function explorer() {
   document.getElementById("output").innerHTML = "explorer works";
