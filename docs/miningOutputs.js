1b8c30585d775aca67c846dab32eb458ee94ae91
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-17 08:56:37 -0500

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,16 +1,14 @@
 var output = 0;
 var currentStone = 0;
+var currentDrills = localStorage.getItem(currentDrills);
+var drillSpeed = localStorage.getItem(drillSpeed);
 
 function outputTimer() {
-  setInterval(outputDeterminer, 100);
+  setInterval(outputDeterminer, 11000-100*(currentDrills+100*(drillSpeed+1)));
   document.getElementById("debugTimerOutput").innerHTML = "output timer is functioning";
 }
 
 function outputDeterminer() {
-/* output = new Math.floor((Math.random()*100)+1);
-  if (output !== 1) { */
    ++ currentStone 
-// }
   document.getElementById("visibleOutput").innerHTML = currentStone;
- // document.getElementById("debugOutput").innerHTML = output;
 }
