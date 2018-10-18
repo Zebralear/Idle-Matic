7a7c39d69a4d6efa8e0fcf84d4c3ab8c5330f049
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-18 10:15:11 -0500

    Attempting to add some saving features

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -1,4 +1,4 @@
-var drillNumber = 1;
+var drillNumber = localStorage.getItem(drillNumber);
 var drillSpeed = 1;
 var drillRarity = 1; 
 var drillDoubleDrops= 0;
@@ -10,11 +10,13 @@ localStorage.setItem(dropsPerSecond, drillNumber*(drillSpeed+1));
 
 function buyExtra() {
   if (money >= 1000*drillNumber) {
-    ++ drillNumber
+    ++ drillNumber;
     money = money-(1000*(drillNumber-1));
+    localStorage.setItem(drillNumber, drillNumber);
   }
   if (drillNumber == 2) {
     unlockedSpeed = 1;
+    localStorage.setItem(unlockedSpeed, unlockedSpeed);
     document.getElementById("drillSpeed").style.display = "block";
     }  
   document.getElementById("drillTracker").innerHTML = drillNumber;
