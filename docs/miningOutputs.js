a9cb1e241695e971d5f5ad85cb05946c606571fb
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-17 19:13:15 -0500

    Did some shenanigans with Local Storage
    An attempt was made to a system to *not* reset your stone whenever the page is loaded

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,9 +1,12 @@
 var output = 0;
 var currentStone = 0;
 var dropRate = localStorage.getItem(dropsPerSecond);
+window.onload(stoneGetter) {
+  currentStone = localStorage.getItem(stone);
+}
 
 function outputTimer() {
-  setInterval(outputDeterminer, 10000/dropRate);
+  setInterval(outputDeterminer, 10000/dropRate+1);
   document.getElementById("debugTimerOutput").innerHTML = "output timer is functioning";
 }
 
