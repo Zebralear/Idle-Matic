d6a00e022b426122f12184f5340ac64a5b689067
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-10-21 12:56:09 -0500

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,4 +1,4 @@
-localStorage.setItem(stone, 100);
+window.localStorage.setItem(stone, 100);
 var currentStone = localStorage.getItem(stone);
 document.getElementById("lSDebugger").innerHTML = localStorage.getItem(stone);
 var dropRate = localStorage.getItem(dropsPerSecond);
