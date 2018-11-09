598e930a2900e0af5db0e47d6225937d2654b2ce
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-11-09 07:49:08 -0600

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -1,21 +1,28 @@
+// Changelog
 var changeLog = "Changeset 0.0.";
-var changeLogger = "39";
+var changeLogger = "40";
 
+// Does stuff
+localStorage.setItem(stone, 100);
 document.getElementById("changeLog").innerHTML = changeLog + changeLogger;
 window.onLoad = function() {storageSetter()};
 var dropRate = localStorage.getItem(dropsPerSecond);
-                 
+document.getElementById("lSDebugger").innerHTML = localStorage.getItem(stone);
+  
+// Does stuff
 function storageSetter() {
-  window.localStorage.setItem(stone, 100);
+  localStorage.setItem(stone, 100);
   var currentStone = localStorage.getItem(stone);
   document.getElementById("lSDebugger").innerHTML = localStorage.getItem(stone);
 }
 
+// Sets the output interval
 function outputTimer() {
   setInterval(outputDeterminer, 5000);
   document.getElementById("debugTimerOutput").innerHTML = "output timer is functioning";
 }
 
+// Actually does the outputing
 function outputDeterminer() {
    ++ currentStone;
   document.getElementById("visibleOutput").innerHTML = currentStone;
