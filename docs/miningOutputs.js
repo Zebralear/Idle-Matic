5690551a0d0736b869714e682ef435039e6c849c
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-11 08:50:22 -0600

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -2,20 +2,17 @@
 var txt = "Active Changeset ";
 var majorAPI = "0";
 var minorChange = "0";
-var patch = "44";
+var patch = "45";
 var fullMajor = majorAPI + ".";
 var fullMinor = minorChange + ".";
 document.getElementById("changeLog").innerHTML = txt + fullMajor + fullMinor + patch;
 
 /* Does stuff */
 localStorage.setItem(stone, 100);
-document.getElementById("lSDebugger").innerHTML = localStorage.getItem(stone);
 window.setInterval(outputDeterminer, 5000);
-document.getElementById("debugTimerOutput").innerHTML = "output timer is functioning";
 
 /* Does the outputs */
 function outputDeterminer() {
-  ++currentStone
-  document.getElementById("visibleOutput").innerHTML = currentStone;
-  //localStorage.setItem(stone, currentStone);
+  document.getElementById("visibleOutput").innerHTML = ++currentStone;
+  localStorage.setItem(stone, currentStone);
 }
