79d4f48b84ea877ee5d2fde4b3ae33eb2cfdcc74
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-13 07:39:05 -0600

    Update miningUpgrades.js

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -1,3 +1,14 @@
+/* Changelog */
+var txt = "Active Changeset";
+var majorAPI = "0";
+var minorFeature = "0";
+var patch = "7";
+var fullAPI = majorAPI + ".";
+var fullFeature = minorFeature + ".";
+document.getElementById("changeLog").innerHTML = fullAPI + fullFeature + patch;
+
+
+/* YAY, Variables!! and sarcasm */
 var drillNumber = 0;
 var drillSpeed = 1;
 var drillRarity = 1; 
@@ -10,33 +21,24 @@ var money = 100000000;
 
 localStorage.setItem(dropsPerSecond, (drillNumber*(drillSpeed+1)));
 
-// Changelog
-var txt = "Active Changeset";
-var majorAPI = "0";
-var minorFeature = "0";
-var patch = "6";
-var fullAPI = majorAPI + ".";
-var fullFeature = minorFeature + ".";
-document.getElementById("changeLog").innerHTML = fullAPI + fullFeature + patch;
+/* Responsible for shit being done when the page loads. */
+  document.getElementById("drillTracker").innerHTML = localStorage.getItem("drills");
 
-// Responsible for shit being done when the page loads.
-  document.getElementById("drillTracker").innerHTML = localStorage.getItem(drillNumber);
-
-// Responsible for the extra drills being suddenly created from nothing.
+/* Responsible for the extra drills being suddenly created from nothing. */
 function buyExtra() {
   if (money >= 1000*drillNumber) {
     money = money-(1000*drillNumber);
     ++ drillNumber;
-    localStorage.setItem(drills, drillNumber);
+    localStorage.setItem("drills", drillNumber);
     document.getElementById("moneyTracker").innerHTML = money;
   }
   if (drillNumber == 2) {
     unlockedSpeed = 1;
-    localStorage.setItem(unlockedSpeed, 1);
+    localStorage.setItem("unlockedSpeed", 1);
     document.getElementById("drillSpeed").style.display = "block";
   }  
   document.getElementById("drillTracker").innerHTML = drillNumber;
 }
-// Responsible for the space-time anomalies that dilate time around your drills.
+/* Responsible for the space-time anomalies that dilate time around your drills. */
 function buySpeed() {
 }
