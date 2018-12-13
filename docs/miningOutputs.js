6adfa30164141163af0d69f2d303eab54f75ab73
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-12 20:50:02 -0600

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -2,7 +2,7 @@
 var txt = "Active Changeset ";
 var majorAPI = "0";
 var minorChange = "0";
-var patch = "52";
+var patch = "53";
 var fullMajor = majorAPI + ".";
 var fullMinor = minorChange + ".";
 document.getElementById("changeLog").innerHTML = txt + fullMajor + fullMinor + patch;
