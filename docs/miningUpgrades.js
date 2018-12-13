17bab658a73c6f1cf747477609d40e7abbc86d68
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-13 08:36:58 -0600

    Update miningUpgrades.js

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -2,10 +2,10 @@
 var txt = "Active Changeset";
 var majorAPI = "0";
 var minorFeature = "0";
-var patch = "9";
+var patch = "10";
 var fullAPI = majorAPI + ".";
 var fullFeature = minorFeature + ".";
-document.getElementById("changeLog").innerHTML = fullAPI + fullFeature + patch;
+document.getElementById("changeLog").innerHTML = txt + fullAPI + fullFeature + patch;
 
 
 /* YAY, Variables!! and sarcasm */
@@ -21,6 +21,7 @@ var money = 100000000;
 
 /* Responsible for shit being done when the page loads. */
 localStorage.setItem("dropsPerSecond", (drillNumber*(drillSpeed+1)));
+localStorage.setItem("drills", drillNumber);
 
 function getDrillNumber() {
   document.getElementById("uOutput").innerHTML = localStorage.drills;
@@ -30,15 +31,11 @@ function getDPS() {
   document.getElementByIs("uOutput").innerHTML = localStoage.dropsPerSecond;
 }
 
-
-
-
 /* Responsible for the extra drills being suddenly created from nothing. */
 function buyExtra() {
-  if (money >= 1000*drillNumber) {
-    money = money-(1000*drillNumber);
-    ++ drillNumber;
-    localStorage.setItem("drills", drillNumber);
+  if (money >= 1000*localStorage.drills) {
+    money = money-(1000*localStorage.drills);
+    ++ localStorage.drills;
     document.getElementById("moneyTracker").innerHTML = money;
   }
   if (drillNumber == 2) {
@@ -46,7 +43,7 @@ function buyExtra() {
     localStorage.setItem("unlockedSpeed", 1);
     document.getElementById("drillSpeed").style.display = "block";
   }  
-  document.getElementById("drillTracker").innerHTML = drillNumber;
+  document.getElementById("drillTracker").innerHTML = localStorage.drills;
 }
 /* Responsible for the space-time anomalies that dilate time around your drills. */
 function buySpeed() {
