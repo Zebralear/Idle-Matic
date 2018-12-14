bc52917b70a46ccc7b0345d60ad0442a18ebfa91
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-14 09:22:19 -0600

    Update miningOutputs.js

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -2,7 +2,7 @@
 var txt = "Active Changeset ";
 var majorAPI = "0";
 var minorChange = "0";
-var patch = "70";
+var patch = "71";
 var fullMajor = majorAPI + ".";
 var fullMinor = minorChange + ".";
 document.getElementById("changeLog").innerHTML = txt + fullMajor + fullMinor + patch;
@@ -10,8 +10,9 @@ document.getElementById("changeLog").innerHTML = txt + fullMajor + fullMinor + p
 /* Does stuff */
 window.onLoad("stoneDefiner");
 window.onLoad("lsDebugger");
-window.setInterval(outputDeterminer, 5000);
-localStorage.setItem("stone", 1)
+window.setInterval(stoneDefiner, 5000);
+window.setInterval(specialDropper, 5000);
+localStorage.setItem("stone", 1);
 
 /* Dynamic Definer for currentStone */
 function stoneDefiner() {
@@ -25,11 +26,7 @@ function stoneDefiner() {
     document.getElementById("uOutput").innerHTML = localStorage.getItem("stone");
   }
 }
-
-
-
-/* lsDebugger */
-function lsDebugger() {
-  document.getElementById("visibleOutput").innerHTML = localStorage.getItem("stone");
-  document.getElementById("uOutput").innerHTML = localStorage.getItem("stone");
+/* Special, Shiny Loot Dropper */
+function specialDropper() {
 }
+  
