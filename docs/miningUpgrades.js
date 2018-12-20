9c05b78fe52e8828377b1c160cbc02bb4af6e13c
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-20 07:57:33 -0600

    Update miningUpgrades.js

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -18,8 +18,6 @@ var money = 100000000;
 
 /* Responsible for shit being done when the page loads. */
 localStorage.setItem("dropsPerSecond", (drillNumber*(drillSpeed+1)));
-
-
 function getDPS() {
   document.getElementById("uOutput").innerHTML = localStoage.getItem("dropsPerSecond");
 }
@@ -28,20 +26,18 @@ function getDPS() {
 function buyExtra() {
   if (money >= 1000*localStorage.drills) {
     money = money-(1000*localStorage.drills);
-    if (typeof(Storage) !== "undefined") {
       if (localStorage.drills) {
         localStorage.drills = Number(localStorage.drills)+1;
       } else {
         localStorage.drills = 1;
       }
       document.getElementById("moneyTracker").innerHTML = money;
-    }
+  }
     if (localstorage.drills == 2) {
       localStorage.setItem("unlockedSpeed", 1);
       document.getElementById("drillSpeed").style.display = "block"
     }
   document.getElementById("drillTracker").innerHTML = localStorage.drillCount;
-  }
 }
 /* Responsible for the space-time anomalies that dilate time around your drills. */
 function buySpeed() {
