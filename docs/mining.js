f02047a564dcee9c503de774e5a0ca900b614a58
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-20 07:38:20 -0600

    Update mining.js

--- docs/mining.js
+++ docs/mining.js
@@ -6,3 +6,6 @@ function miningUpgradeLoad() {
   $("#classInternalPlacer").load("miningUpgradeInternal.html");
   document.getElementById("classInternalPlacer").style.backgroundImage = "radial-gradient(circle, 7B9095, CACCCE)";
 }
+function miningShinyLoad() {
+  $("#classInternalPlacer").load("miningShiny.html");
+}
