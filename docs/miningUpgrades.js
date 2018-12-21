a8500a2bed8e07e6536778304c6e1de0e15b6654
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-21 08:01:30 -0600

    Update miningUpgrades.js

--- docs/miningUpgrades.js
+++ docs/miningUpgrades.js
@@ -2,7 +2,7 @@
 var txt = "Active Changeset ";
 var majorAPI = "0";
 var minorFeature = "0";
-var patch = "20";
+var patch = "21";
 var fullAPI = majorAPI + ".";
 var fullFeature = minorFeature + ".";
 document.getElementById("changeLog").innerHTML = txt + fullAPI + fullFeature + patch;
