7462c9dcb170acae03c4aecd687ac48c108c1b5b
Zebralear <31323325+Zebralear@users.noreply.github.com>
2018-12-21 09:32:53 -0600

    Wait...
    I just remembered something!
    Going from X.0.x to X.1.x resets ‘x’ to 0...
    Silly me!

--- docs/miningOutputs.js
+++ docs/miningOutputs.js
@@ -2,7 +2,7 @@
 var txt = "Active Changeset ";
 var majorAPI = "0";
 var minorChange = "1";
-var patch = "76";
+var patch = "3";
 var fullMajor = majorAPI + ".";
 var fullMinor = minorChange + ".";
 document.getElementById("changeLog").innerHTML = txt + fullMajor + fullMinor + patch;
